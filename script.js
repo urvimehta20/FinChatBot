180ac896af2181b79eb6ff588a8a2c05c7183276
URVI MEHTA <80567458+urvimehta20@users.noreply.github.com>
2023-06-08 19:38:16 +0530

    Updating the script.js to change the heading element

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function() {
     // Get the heading element by its ID
     var heading = document.getElementById('heading');
   
+    
     // Change the text content of the heading
     heading.textContent = 'Your Financial Chat Companion';
   };
