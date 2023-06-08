8d17784e67dcaa5ff2adc8a0fd07dcaf9b212888
URVI MEHTA <80567458+urvimehta20@users.noreply.github.com>
2023-06-08 19:31:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    // Get the heading element by its ID
+    var heading = document.getElementById('heading');
+  
+    // Change the text content of the heading
+    heading.textContent = 'Your Financial Chat Companion';
+  };
+  
